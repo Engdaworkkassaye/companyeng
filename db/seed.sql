ef36153f9e41970b410f29d59fd71e1b97e5df88
Engdawork Kassaye <engdaworkkassaye6@gmail.com>
2024-02-01 14:18:12 -0600

    added seed

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO departments (name) VALUES ('Development'), ('Testing');
+INSERT INTO roles (title, salary, department_id) VALUES 
+
+('Developer', 50000, 1),
+('tester', 60000, 2);
+
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES 
+('Engda', 'Minas', 3, 2),
+('Abebe', 'Abebe', 2, 1);
+
